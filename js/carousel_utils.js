129c4d315090132ef6f753928ab0c6778d2c46a6
Justin Kerr <justin.g.kerr@gmail.com>
2024-09-24 14:25:32 -0700

    minor

--- js/carousel_utils.js
+++ js/carousel_utils.js
@@ -1,19 +1,9 @@
-// For teaser object carousel -- left/right arrow clicks to navigate
-function teaser_slide_left() {
-  slider_window = document.getElementById('teaser-objs-scroll');
-  slider_window.scrollLeft -= 200;
-}
-function teaser_slide_right() {
-  slider_window = document.getElementById('teaser-objs-scroll');
-  slider_window.scrollLeft += 200;
-}
-
 // For main results object carousel -- left/right arrow clicks to navigate
 function results_slide_left() {
   slider_window = document.getElementById('results-objs-scroll');
-  slider_window.scrollLeft = 0;
+  slider_window.scrollLeft -= 150;
 }
 function results_slide_right() {
   slider_window = document.getElementById('results-objs-scroll');
-  slider_window.scrollLeft += 1000;
+  slider_window.scrollLeft += 150;
 }
