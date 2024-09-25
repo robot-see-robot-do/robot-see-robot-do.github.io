a79987659fe6236bd21da76d6b623b0328db353f
Justin Kerr <justin.g.kerr@gmail.com>
2024-09-24 17:19:54 -0700

    styling of 3d stuff

--- script.js
+++ script.js
@@ -25,6 +25,7 @@ $(() => {
 });
 // Array of iframe IDs
 const iframeIds = ['bear', 'nerfgun', 'scissors','sunglasses','redbox','ledlight','stapler','wirecutters','usbplug'];
+const videoIds = ['bear_video', 'nerfgun_video', 'redbox_video', 'scissors_video', 'sunglasses_video', 'ledlight_video', 'stapler_video', 'wirecutters_video', 'usbplug_video'];
 
 // Function to show the selected iframe and hide others
 function showIframe(iframeId) {
@@ -40,6 +41,22 @@ function showIframe(iframeId) {
     }
   });
 }
+// Function to show the selected video and hide others
+function showVideo(videoId) {
+  videoIds.forEach(id => {
+    const video = document.getElementById(id);
+    if (video) {
+      if (id === videoId) {
+        video.classList.add('show');
+        video.querySelector('video').play(); // Play the selected video
+      } else {
+        video.classList.remove('show');
+        video.querySelector('video').pause(); // Pause other videos
+        video.querySelector('video').currentTime = 0; //Also restart all others
+      }
+    }
+  });
+}
 
 // Function to set up thumbnail click events
 function setupThumbnailClickEvents() {
@@ -48,6 +65,7 @@ function setupThumbnailClickEvents() {
     thumbnail.addEventListener('click', function() {
       const iframeId = this.id.replace('-thumb', '');
       showIframe(iframeId);
+      showVideo(iframeId + '_video');
     });
   });
 }
@@ -59,6 +77,9 @@ function initializePage() {
   // Show the first iframe by default
   if (iframeIds.length > 0) {
     showIframe(iframeIds[0]);
+  }
+  if (videoIds.length > 0) {
+    showVideo(videoIds[0]);
   }
 }
 // Run initialization when the DOM is fully loaded
